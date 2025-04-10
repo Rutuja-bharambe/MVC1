4d273be31f8d3d4a32e06378ddd0260e19a66cfc
Rutuja-bharambe <r.bharambe@somaiya.edu>
2025-04-10 20:17:44 +0530

    Initial commit

---
+++ MVC1/MVC1/Controllers/HelloController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC1.Models;
+
+namespace MVC1.Controllers
+{
+    public class HelloController : Controller
+    {
+        public IActionResult Index()
+        {
+            TempData["Location"] = "Mumbai";
+            return View();
+        }
+        public IActionResult Student()
+        {
+            Student student = new Student()
+            {
+                Id = 1,
+                Name = "Vibha",
+                Gender = "Female",
+            };
+            ViewBag.CurrentDate = DateTime.Now.ToShortDateString();
+            ViewData["Title"] = "Mr";
+            String? location = Convert.ToString(TempData["Location"]);
+
+
+            return View(student);
+
+        }
+
+        }
+
+        }
+    
+
